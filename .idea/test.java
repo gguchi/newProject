e3a288bd127660f81e045efb16db6da528dc72d9
Norimar Iguchi <gguchi193@gmail.com>
2021-03-25 20:52:55 +0800

    Initial commit

---
+++ .idea/test.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+        //Primitive
+        int wholeNumbers = 122;
+        long largeDecimalNumbers = -12;
+
+        System.out.println("% and %", &wholeNumbers, &largeDecimalNumbers);
+
+        //Print all primitive typse, output and run
+
+        //Non primitive
+
+    }
+
+}
